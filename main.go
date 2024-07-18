f88cc46105d560d1e4b97a7e5c4676059f5e9354
Joel <triwira.joel@gmail.com>
2024-07-18 14:35:40 +0700

    add endpoints

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -28,16 +27,32 @@ func main() {
 	handler := h.NewHttpHandler(*usecase)
 
 	e := echo.New()
+
+	u := e.Group("/users")
+	j := e.Group("/jobs")
+	a := e.Group("/applications")
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.GET("/user/:id", handler.GetUser)
-	e.GET("/user", handler.GetUsers)
-	e.POST("/user", handler.CreateUser)
+	u.GET("/:id", handler.GetUser)
+	u.GET("/", handler.GetUsers)
+	u.POST("/", handler.CreateUser)
 
-	fmt.Println(database)
+	j.GET("/", handler.GetJobs)
+	j.GET("/:id", handler.GetJob)
+	j.GET("/:employer_id", handler.GetJobsByEmployerID)
+	j.POST("/", handler.CreateJob)
+
+	a.GET("/", handler.GetApplications)
+	a.GET("/:id", handler.GetApplication)
+	a.GET("/:talent_id", handler.GetApplicationsByTalentID)
+	a.GET("/:employer_id", handler.GetJobsByEmployerID)
+	a.POST("/", handler.CreateApplication)
+	a.PUT("/:id", handler.UpdateApplicationStatus)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }

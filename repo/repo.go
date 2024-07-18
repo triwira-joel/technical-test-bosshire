25765d01a091e8143be8bea3f10bed8b17c441cc
Joel <triwira.joel@gmail.com>
2024-07-18 14:33:36 +0700

    add all function for job and application

--- repo/repo.go
+++ repo/repo.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/triwira-joel/technical-test-bosshire/db"
 	d "github.com/triwira-joel/technical-test-bosshire/domain"
@@ -28,8 +30,8 @@ func (r *Repo) GetUser(c echo.Context, id int) (*d.User, error) {
 	return user, nil
 }
 
-func (r *Repo) CreateUser(c echo.Context, name string, role string, email string) (*d.User, error) {
-	newUser := d.NewUser(name, role, email)
+func (r *Repo) CreateUser(c echo.Context, name string, role string, email string, password string) (*d.User, error) {
+	newUser := d.NewUser(name, role, email, password)
 
 	if err := r.db.CreateUser(newUser); err != nil {
 		return nil, err
@@ -45,4 +47,107 @@ func (r *Repo) GetUsers(c echo.Context) ([]*d.User, error) {
 	}
 
 	return users, nil
+}
+
+func (r *Repo) GetJobs(c echo.Context) ([]*d.Job, error) {
+	jobs, err := r.db.SelectJobs()
+	if err != nil {
+		return nil, err
+	}
+
+	return jobs, nil
+}
+
+func (r *Repo) CreateJob(c echo.Context, name string, description string, employer_id int) (*d.Job, error) {
+	newJob := d.NewJob(name, description, employer_id)
+
+	if err := r.db.CreateJob(newJob); err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return newJob, nil
+}
+
+func (r *Repo) GetJob(c echo.Context, id int) (*d.Job, error) {
+	job, err := r.db.SelectJobByID(id)
+	fmt.Println("REPO: ", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (r *Repo) GetJobsByEmployerID(c echo.Context, id int) ([]*d.Job, error) {
+	jobs, err := r.db.SelectJobByEmployerID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return jobs, nil
+}
+
+func (r *Repo) GetApplications(c echo.Context) ([]*d.Application, error) {
+	applications, err := r.db.SelectApplications()
+	if err != nil {
+		return nil, err
+	}
+
+	return applications, nil
+}
+
+func (r *Repo) GetApplication(c echo.Context, id int) (*d.Application, error) {
+	application, err := r.db.SelectApplicationByID(id)
+	fmt.Println("-- MASUK REPO --")
+	if err != nil {
+		return nil, err
+	}
+
+	return application, nil
+}
+
+func (r *Repo) GetApplicationsByEmployerID(c echo.Context, employer_id int) ([]*d.Application, error) {
+	applications, err := r.db.SelectApplicationsByEmployerID(employer_id)
+	if err != nil {
+		return nil, err
+	}
+
+	return applications, nil
+}
+
+func (r *Repo) GetApplicationsByTalentID(c echo.Context, talent_id int) ([]*d.Application, error) {
+	applications, err := r.db.SelectApplicationsByTalentID(talent_id)
+	if err != nil {
+		return nil, err
+	}
+
+	return applications, nil
+}
+
+func (r *Repo) CreateApplication(c echo.Context, job_id, talent_id, employer_id int) (*d.Application, error) {
+	newApplication := d.NewApplication(job_id, talent_id, employer_id, string(d.Review))
+
+	if err := r.db.CreateApplication(newApplication); err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return newApplication, nil
+}
+
+func (r *Repo) UpdateApplicationStatus(c echo.Context, id int, status string) (*d.Application, error) {
+	application, err := r.db.SelectApplicationByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	application.Status = status
+
+	if err := r.db.UpdateApplicationStatus(application); err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return application, nil
 }
